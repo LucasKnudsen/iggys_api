73c95f28795596e844b667054697acdf76b9cf15
Lucas Knudsen <laksteelhouse@gmail.com>
2021-03-25 09:51:23 +0100

    Adds Message model

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,15 +1,21 @@
 class Api::MessagesController < ApplicationController
   before_action :authenticate_user!, only: :create
 
-  def index 
+  def index
     messages = Message.all
-    render json: {messages: messages}
+    render json: { messages: messages }
   end
 
   def create
-    current_user.messages.create(content: params[:content])
-    render json: {
-      message: 'Successful'
-    }, status: 201
+    if params[:content]
+      current_user.messages.create(content: params[:content])
+      render json: {
+        message: 'Successful'
+      }, status: 201
+    else
+      render json: {
+        message: 'Unsuccessful'
+      }, status: 422
+    end
   end
 end
